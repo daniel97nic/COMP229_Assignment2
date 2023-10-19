4718803369ce65f812930628c360a0b7ed34132f
Daniel Mayorga <daniel.mayorga97@hotmail.com>
2023-10-19 18:32:12 -0400

    all working 1

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -65,6 +65,20 @@ const ProductController = {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
+  },
+  getProductByName: async (req, res) => {
+    try {
+      const partialProductName = req.query.name;
+      const products = await Product.find({ name: { $regex: partialProductName, $options: 'i' } });
+      
+      if (products.length > 0) {
+        res.status(200).json(products);
+      } else {
+        res.status(404).json({ error: 'No products found with the provided partial name parameter.' });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 };
 
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -5,11 +5,14 @@ const ProductController = require('../controllers/product.controller');
 // Create a new product
 router.post('/products', ProductController.createProduct);
 
-// Get all products
-router.get('/products', ProductController.getAllProducts);
-
 // Get product by ID
 router.get('/products/:id', ProductController.getProductById);
+
+//Get product by partial name
+router.get('/products', ProductController.getProductByName);
+
+// Get all products
+router.get('/products', ProductController.getAllProducts);
 
 // Update product by ID
 router.put('/products/:id', ProductController.updateProduct);
@@ -17,7 +20,7 @@ router.put('/products/:id', ProductController.updateProduct);
 // Delete product by ID
 router.delete('/products/:id', ProductController.deleteProduct);
 
-//Delete all products
+// Delete all products
 router.delete('/products', ProductController.deleteAllProducts);
 
 module.exports = router;
